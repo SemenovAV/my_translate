ef03763654411c2965ad2a6e232decfd1823e861
Aleksey Semenov <7.on.off@bk.ru>
2020-01-25 15:24:48 +0300

    Реализация.
    Реализует основной функционал.

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
-import requests
-#  документация https://yandex.ru/dev/translate/doc/dg/reference/translate-docpage/
-
-API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
-URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-
-def translate_it(text, to_lang):
-    """
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-    :param to_lang:
-    :return:
-    """
-
-    params = {
-        'key': API_KEY,
-        'text': text,
-        'lang': 'ru-{}'.format(to_lang),
-    }
-
-    response = requests.get(URL, params=params)
-    json_ = response.json()
-    return ''.join(json_['text'])
-
-
-# print(translate_it('В настоящее время доступна единственная опция — признак включения в ответ автоматически определенного языка переводимого текста. Этому соответствует значение 1 этого параметра.', 'no'))
+import os
+from tools.logger import MyLogger
+from translate_it import translate_it
 
 if __name__ == '__main__':
-    print(translate_it('привет', 'en'))
+    directories = 'data'
+    result_directories = 'translates'
+    from_lng = 'ru'
+    log_file = os.path.join(result_directories, 'translate.log')
+    for file in os.listdir(directories):
+        name = file.split('.')[0]
+        result_name = f'{name}-translate-{from_lng}.txt'
+        with MyLogger(log_file):
+            translate_it(
+                os.path.join(directories, file),
+                os.path.join(result_directories, result_name),
+                from_lng,
+                name
+            )
